8ba89510de0bf1dfd39e1a99a9fc9093cb7fbd3a
Massimo Chen <haomassimochen@gmail.com>
2022-09-21 14:11:15 -0400

    Done with required stories
    -User will be able to see the detail page
    -The detail page includes the youtube trailer of the movie clicked
    - It will show the title overview and the rating of the movie

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,16 +1,18 @@
 package com.example.flixster
 
-import android.content.Context
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
-
+@Parcelize
 data class Movie(
     val movieId: Int,
+    val voteAverage: Double,
     val posterPath: String,
     val backdropPath: String,
     val title: String,
     val overview: String,
-){
+): Parcelable{
 
 
 
@@ -22,6 +24,7 @@ data class Movie(
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("backdrop_path"),
                         movieJson.getString("title"),
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -2,6 +2,7 @@ package com.example.flixster
 
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,11 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>):
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -31,7 +33,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() =  movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
@@ -48,6 +50,18 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             Glide.with(context).load(posterImageUrl).placeholder(R.drawable.placeholder).error(R.drawable.error).into(ivPoster)
             Log.i("orient2", context.resources.configuration.orientation.toString())
 
+        }
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+        override fun onClick(p0: View?) {
+//            1. Get notified of the particular movie which was clicked
+            val movie = movies[adapterPosition]
+//            2. Use intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA,movie)
+            context.startActivity(intent)
         }
     }
 }
